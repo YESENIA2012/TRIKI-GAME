5a55ff7d75dc7a31668da8089640a692d64ca3a5
YESENIA2012 <gyesenia173@gmail.com>
2021-12-01 22:23:49 -0500

    Track every player move into a matriz

--- creatingGame.js
+++ creatingGame.js
@@ -19,6 +19,8 @@ function matriz() {
   let numFilas = document.getElementById("filas").value;
   let numColumnas = document.getElementById("columnas").value;
 
+  var arrayGanador = [];
+
   for (a = 0; a < numFilas; a++) {
     let divPadre = document.querySelector(".padre");
     let rowElement = document.createElement("div");
@@ -27,16 +29,26 @@ function matriz() {
     rowElement.setAttributeNode(atributo);
     divPadre.appendChild(rowElement);
 
+    arrayGanador[a] = [];
+
     for (b = 0; b < numColumnas; b++) {
       let columnElement = document.createElement("div");
       let atributo2 = document.createAttribute("class");
-      atributo2.value = "column row" + [a + 1] + "-" + "column" + [b + 1];
+      atributo2.value = "column";
       columnElement.setAttributeNode(atributo2);
+
       let imagen = document.createElement("img");
+      let atributoImagen = document.createAttribute("class");
+      atributoImagen.value = "row" + "-" + [a] + "-" + [b];
+      imagen.setAttributeNode(atributoImagen);
+
       columnElement.appendChild(imagen);
       rowElement.appendChild(columnElement);
+
+      arrayGanador[a][b] = "";
     }
   }
+
   let padre = document.querySelector(".padre");
   padre.addEventListener("click", insertarX);
 
@@ -45,6 +57,13 @@ function matriz() {
   function insertarX(evento) {
     let imagen = evento.target;
 
+    var atributosImagen = imagen.getAttribute("class");
+    console.log("esta es la cadena", atributosImagen);
+
+    let cadena = atributosImagen.split("-");
+    console.log("esta es la cadena", cadena);
+    console.log("número de fila", cadena[1], "número de columna", cadena[2]);
+
     if (imagen.hasAttribute("src")) {
       alert("La columna ya esta en uso");
     } else {
@@ -52,10 +71,14 @@ function matriz() {
         imagen.style.opacity = "1";
         imagen.src = "x-icon.png";
         turno = jugador2;
+        arrayGanador[cadena[1]][cadena[2]] = "X";
+        console.log(arrayGanador);
       } else {
         imagen.style.opacity = "1";
         imagen.src = "o2-icon.png";
         turno = jugador1;
+        arrayGanador[cadena[1]][cadena[2]] = "O";
+        console.log(arrayGanador);
       }
     }
   }
